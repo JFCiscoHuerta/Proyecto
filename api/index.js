19e86ca544e9a7f8b5b3e0ed51aeb4f38447a55f
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2024-10-26 21:42:48 -0600

    feat: add endpoints

--- api/index.js
+++ api/index.js
@@ -4,6 +4,10 @@ import helmet from "helmet"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
 
+import UserController from "./controllers/UserController.js"
+import EventsController from "./controllers/EventsController.js"
+import TeamsController from "./controllers/TeamsController.js"
+
 dotenv.config();
 
 const app = express()
@@ -13,9 +17,20 @@ mongoose.connect(process.env.MONGO_URL)
 
 app.use(cors())
 app.use(helmet())
+app.use(express.json())
 
 app.get("/", (req, res) => {
     res.send("Hola")
 })
+
+//Users
+app.post("/user/register", UserController.register)
+app.post("/user/login", UserController.login)
+app.put("/user/update-profile/:id", UserController.updateProfile)
+//Events
+app.post("/event/create", EventsController.createEvent)
+//Teams
+app.post("/team/create", TeamsController.createTeam)
+app.post("/events/:idEvent/register/:id", TeamsController.registerEvent);
 
 app.listen(4000, () =>console.log("Server is running"))
